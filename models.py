c8fb20c7796e5b9f9712fcee6a0292bc97ed9515
mikkyhu <myhu@princeton.edu>
2020-12-04 14:34:40 -0500

    ipynb for calculating similarities. pretty fast.

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class RegressionBase(nn.Module):
     def __init__(self, input_dim, embedding_dim):
         super().__init__()
         self.embedding = nn.Embedding(input_dim, embedding_dim)
+        self.embedding.weight.requires_grad=False
 
 class Linear(RegressionBase):
     def __init__(self, input_dim, embedding_dim, output_dim):
@@ -29,7 +30,8 @@ class MLP1(RegressionBase):
     def forward(self, x):
         x.transpose_(1, 0)
         x = self.embedding(x)
-        x = torch.flatten(x)
+        x = torch.flatten(x, start_dim=1)
+        # print(x.shape)
         x = self.l1(x)
         x = F.relu(x)
         x = self.l2(x)
@@ -45,7 +47,7 @@ class MLP2(RegressionBase):
     def forward(self, x):
         x.transpose_(1, 0)
         x = self.embedding(x)
-        x = torch.flatten(x)
+        x = torch.flatten(x, start_dim=1)
         x = self.l1(x)
         x = F.relu(x)
         x = self.l2(x)
--- train.py
+++ train.py
@@ -9,11 +9,12 @@ import torchtext
 from torchtext import data
 
 import sklearn
-
-import pyprind
+import wandb
+import os
 
 from models import Linear, MLP1, MLP2
 
+wandb.init(project='meta-analogy')
 
 spacy_en = spacy.load('en')
 
@@ -51,13 +52,14 @@ BATCH_SIZE = 32
 EMBEDDING_DIM = 900
 OUTPUT_DIM = 300
 INPUT_DIM = 300
-N_EPOCHS = 5
+N_EPOCHS = 100
+wandb.config.update({"batch_size": BATCH_SIZE})
 
 ########################################################
 # END HYPERPARAMETERS
 ########################################################
 
-device = torch.device('cpu')
+device =  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits((train_data, valid_data, test_data), 
     batch_size=BATCH_SIZE, 
@@ -67,10 +69,13 @@ train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits((trai
 # print(TEXT.vocab.freqs)
 
 model = Linear(INPUT_DIM, EMBEDDING_DIM, OUTPUT_DIM)
+# model = MLP1(INPUT_DIM, EMBEDDING_DIM, 1000, OUTPUT_DIM)
 pretrained_embeddings = TEXT.vocab.vectors
+wandb.watch(model)
 # print(pretrained_embeddings[500])
 
 model.embedding.weight.data = pretrained_embeddings
+model.to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 criterion = nn.MSELoss(reduction='sum')
@@ -81,9 +86,8 @@ def train(model, iterator, optimizer, criterion):
     
     model.train()
 
-    bar = pyprind.ProgBar(len(iterator))
     for batch in iterator:
-        if batch.Text.size(0) != 3:
+        if batch.Text.size(0) != 3: # don't consider multi-word analogies
             continue
         optimizer.zero_grad()
 
@@ -96,7 +100,6 @@ def train(model, iterator, optimizer, criterion):
         optimizer.step()
 
         epoch_loss += loss.item()
-        bar.update()
     
     return epoch_loss / len(iterator)
 
@@ -105,12 +108,10 @@ def evaluate(model, iterator, criterion):
     
     model.eval()
     with torch.no_grad():
-        bar = pyprind.ProgBar(len(iterator))
         for batch in iterator:
             # print(batch.Text.shape)
-            if batch.Text.size(0) != 3:
+            if batch.Text.size(0) != 3: # don't consider multi-word analogies
                 continue
-            optimizer.zero_grad()
 
             predictions = model(batch.Text)
             labels = model.embedding(batch.Label).squeeze(0)
@@ -118,14 +119,23 @@ def evaluate(model, iterator, criterion):
             loss = criterion(predictions, labels)
 
             epoch_loss += loss.item()
-            bar.update()
         
     return epoch_loss / len(iterator)
+
+best_value = None
 
 for epoch in range(N_EPOCHS):
     train_loss = train(model, train_iterator, optimizer, criterion)
     valid_loss = evaluate(model, valid_iterator, criterion)
     
+    wandb.log({'training loss' : train_loss, 'validation loss': valid_loss})
     print(f'Train loss: {train_loss} | Validation loss: {valid_loss}')
 
-torch.save(model.state_dict(), 'model')
+    if (best_value is None) or (best_value > valid_loss):
+        best_value = valid_loss
+        save_model = True
+    else:
+        save_model = False
+
+    if save_model:
+        torch.save(model.state_dict(), os.path.join(wandb.run.dir, 'model.pt'))
